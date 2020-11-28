facd6f704996235dca59f3971c3004d6040a345d
miladhzz <miladhzz@gmail.com>
2020-11-28 21:26:20 +0330

    complete section 003

--- cbvs/app1/urls.py
+++ cbvs/app1/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('user-list/', views.user_list, name='user_list'),
     path('user-list2/', views.UserList2.as_view(), name='user_list2'),
     path('group-list/', ListView.as_view(model=Group, context_object_name='groups'), name='group_list'),
-    path('group-list2/', views.GroupList.as_view(context_object_name='groups', test11='test2'), name='group_list2'),
+    path('group-list2/', views.GroupList.as_view(context_object_name='groups', test='test2'), name='group_list2'),
+    path('user-list3/', views.UserList3.as_view(), name='user_list3'),
 ]
--- cbvs/app1/views.py
+++ cbvs/app1/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
-from django.views import generic
+from django.views import generic, View
 
 
 def user_list(mmm):
@@ -11,8 +12,29 @@ def user_list(mmm):
     return render(mmm, 'app1/user_list.html', context=context)
 
 
-class UserList2(generic.ListView):
-    model = User
+class UserList2(View):
+    # model = User
+
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        context = {
+            'users': users
+        }
+        return render(request, 'app1/user_list.html', context=context)
+
+    def post(self, request):
+        return HttpResponse("POST")
+
+
+class UserList3(generic.base.TemplateView):
+    template_name = 'app1/user_list.html'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['users'] = User.objects.all()
+    #     return context
+
+    extra_context = {'users': User.objects.all()}
 
 
 class GroupList(generic.ListView):
